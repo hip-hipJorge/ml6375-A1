990fd3a19ff82b144235d72d2ae468eadf2268e7
perez <perez.j1477@gmail.com>
2020-06-10 00:52:38 -0500

    Implemented gradient descent update rule. Needs fixing

--- linear_grad_desc.py
+++ linear_grad_desc.py
@@ -1,7 +1,9 @@
 import array as arr
+import numpy as np
 
+LEARNING_RATE = 0.1
 
-class Line:
+class PopLine:
     def __init__(self, data_value, intercept=0):
         self.pop_size = len(data_value)
         self.data_value = arr.array('d', data_value)
@@ -30,6 +32,8 @@ class Line:
 
     def set_weight(self, weight):
         self.weight = weight
+        # new output
+        self.output = self.calc_output()
 
     def set_intercept(self, intercept):
         self.intercept = intercept
@@ -41,7 +45,7 @@ class Line:
     def calc_output(self):
         self.output = arr.array('d', [0] * self.pop_size)
         for i in range(self.pop_size):
-            self.output[i] = self.weight[i] * self.data_value[i]
+            self.output[i] = self.weight[i] * self.data_value[i] + self.intercept
         return self.output
 
 
@@ -49,6 +53,7 @@ class LinearRegression:
     def __init__(self, line, actual_data):
         self.error = self.calc_error(line.output, actual_data)
         self.mse = self.mean_squared_error(self.error)
+        self.step_size = 0
 
     # getters
     def get_error(self):
@@ -57,12 +62,18 @@ class LinearRegression:
     def get_mse(self):
         return self.mse
 
+    def get_step_size(self):
+        return self.step_size
+
     # setters
     def set_error(self, error):
         self.error = error
 
     def set_mse(self, mse):
         self.mse = mse
+
+    def set_step_size(self, step_size):
+        self.step_size = step_size
 
     # helper functions
     def calc_error(self, hypothesis, actual_value):
@@ -78,20 +89,53 @@ class LinearRegression:
             e_squared_sum += pow(error[i], 2)
         return (1 / (2 * n)) * e_squared_sum
 
+    def new_weight(self, old_weight, data_values, error):
+        n = len(data_values)
+        new_weight = arr.array('d', [0] * n)
+        sum_error_and_data = 0
+        for i in range(n):
+            sum_error_and_data += error[i] * data_values[i]
+        self.set_step_size(LEARNING_RATE * ((-1/n) * sum_error_and_data))
+        for i in range(n):
+            new_weight[i] = old_weight[i] - self.step_size
+        return new_weight
+
+
 
 # test 1 data point
-s = [0.64, 0.64, 0.64]
+w = [0.64, 0.64, 0.64]
 x = [0.5, 2.3, 2.9]
 y = [1.4, 1.9, 3.2]
 
-l = Line(s, x)
-lin_reg = LinearRegression(l, y)
+pl = PopLine(x)
+pl.set_weight(w)
+lin_reg = LinearRegression(pl, y)
 
-print("Output print: %.2f" % (l.get_output()))
-print("Error print:")
-print(*lin_reg.get_error())
-print("MSE print:")
-print(lin_reg.get_mse())
+print("actual:")
+print(y)
+print("output:")
+print(*pl.output)
+print("error:")
+print(*lin_reg.error)
+print("mse:")
+print(lin_reg.mean_squared_error(lin_reg.get_error()))
 
+print()
 
+for i in range(10):
+    print()
+    print("Old weight:")
+    print(*pl.get_weight())
+    pl.set_weight(lin_reg.new_weight(pl.get_weight(),pl.get_data_value(),lin_reg.get_error()))
+    print("New weight:")
+    print(*pl.get_weight())
+    print("new output:")
+    print(*pl.get_output())
+    print("step size:")
+    print(lin_reg.get_step_size())
+    print("new error:")
+    lin_reg.set_error(lin_reg.calc_error(pl.get_output(),y))
+    print(*lin_reg.get_error())
+    print("mse:")
+    print(lin_reg.mean_squared_error(lin_reg.get_error()))
 
